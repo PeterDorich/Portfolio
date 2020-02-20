d8b03a1625f44fa2ad590fc7ab36dbce15a7515a
Peter Dorich <dorich.peter@gmail.com>
2020-02-20 11:48:45 -0800

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-Clean Project repo for Portfolio
-
---This will hold my portfolio itself, and link to other repos containing my projects
-
---
+++ precache-manifest.7352898caee48b3b574c9307c680183f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb1459c750ac70213e16f2dd28bb449",
+    "url": "/Portfolio/src/index.html"
+  },
+  {
+    "revision": "883081216e608f5f87d7",
+    "url": "/Portfolio/src/static/css/main.7e3180f6.chunk.css"
+  },
+  {
+    "revision": "81906e88ae6ca12d0c34",
+    "url": "/Portfolio/src/static/js/2.d16ea994.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Portfolio/src/static/js/2.d16ea994.chunk.js.LICENSE"
+  },
+  {
+    "revision": "883081216e608f5f87d7",
+    "url": "/Portfolio/src/static/js/main.2a3c72f2.chunk.js"
+  },
+  {
+    "revision": "ed70c99ca168338cff54",
+    "url": "/Portfolio/src/static/js/runtime-main.07e9e49e.js"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/Portfolio/src/static/media/GitHub_Logo.cefc2023.png"
+  },
+  {
+    "revision": "021bf5945a7dcba6f3604212411ba26e",
+    "url": "/Portfolio/src/static/media/Resume 2020.021bf594.pdf"
+  },
+  {
+    "revision": "95a418cb6b4c24c6ea838fe701362ffb",
+    "url": "/Portfolio/src/static/media/Valve_System.95a418cb.png"
+  },
+  {
+    "revision": "3ad00a054ebba99f47d4dce89c87e96f",
+    "url": "/Portfolio/src/static/media/adafruit.3ad00a05.png"
+  },
+  {
+    "revision": "744c3aa153050598efac8a6189b060f5",
+    "url": "/Portfolio/src/static/media/arduino.744c3aa1.png"
+  },
+  {
+    "revision": "b89db0651eb24f03b1e7cebd36f2d2aa",
+    "url": "/Portfolio/src/static/media/bike_rental.b89db065.png"
+  },
+  {
+    "revision": "355e3a923775a4967e592a0aafcd0bc9",
+    "url": "/Portfolio/src/static/media/db_model.355e3a92.png"
+  },
+  {
+    "revision": "26c1d84235a72ed3aa80d846d5ad8706",
+    "url": "/Portfolio/src/static/media/p&r_valve.26c1d842.png"
+  },
+  {
+    "revision": "f814d138d5d1c877e41f577f366d6583",
+    "url": "/Portfolio/src/static/media/portfolio.f814d138.png"
+  },
+  {
+    "revision": "ff55b3112863625719a66e3fe9b75bb7",
+    "url": "/Portfolio/src/static/media/profile_pic.ff55b311.jpg"
+  },
+  {
+    "revision": "26894e784c084c56cc136a010c27f14a",
+    "url": "/Portfolio/src/static/media/wireframe.26894e78.jpg"
+  }
+]);
